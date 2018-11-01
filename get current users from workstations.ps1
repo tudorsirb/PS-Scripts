5b84d2c1e8c866e9766ed5fa3536133fef1ddd64
Tudor Sirb <tudor@sirb.net>
2018-11-01 13:22:48 +0200

    fix for exporting to csv

--- get current users from workstations.ps1
+++ get current users from workstations.ps1
@@ -6,7 +6,7 @@ if( (Test-Connection $computer.name -Quiet -count 1)) {
 $lastlogon = (Get-WmiObject -Class win32_process -ComputerName $computer.name | Where-Object name -Match explorer).getowner().user
 $hostname = (invoke-command -ComputerName $computer.name -ScriptBlock {hostname})
  $Obj = New-Object -TypeName PSObject -Property @{
-		"Current user" = $lastlogon
+		"Current user" = $lastlogon -join ','
 		"Hostname" = $hostname		
 }
 Write-Output $Obj

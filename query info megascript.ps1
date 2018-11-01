e55c9e5e8de8466a9901a5e74e4ddfdaff125404
Tudor Sirb <tudor@sirb.net>
2018-11-01 11:53:16 +0200

    first commit for megascript
    Currenty queries for:
    Hostname
    Manufacturer
    Bios version
    Workstation Model
    Serial no
    Last logon for computer account in AD
    Bitlocker status
    Last patch date
    Build no

---
+++ query info megascript.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = 'SilentlyContinue'
+import-module Active*
+$computers = Get-ADComputer -Filter "Name -like 'TIM-*'" | select name
+foreach ($computer in $Computers)
+{if( (Test-Connection $computer.name -Quiet -count 1)) {
+$model = Get-CimInstance Win32_ComputerSystem -ComputerName $computer.name | select Model
+$disk = Get-CimInstance Win32_diskdrive -ComputerName $computer.name | select Model
+$bios = Get-CimInstance Win32_BIOS -ComputerName $computer.name | select SMBIOSBIOSVersion, SerialNumber, Manufacturer
+$lastlog = Get-ADComputer -Identity $computer.name -Properties *
+$lastpatch = get-hotfix -ComputerName $computer.name | sort installedon -desc | select -First 1 installedon
+$buildno = (invoke-command -ComputerName $computer.name -ScriptBlock {(Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('ReleaseID')})
+$BDE = Manage-BDE -ComputerName $Computer.Name -Status C: 
+   If ($BDE -Like "*An error occurred while connecting*") {$Status = "Unable to connect"} 
+   If ($BDE -Like "*Protection On*") {$Status = "Protected"} 
+   If ($BDE -Like "*Protection Off*") {$Status = "Not protected"} 
+    $Obj = New-Object -TypeName PSObject -Property @{
+		"Computer" = $computer.name 
+		"Manufacturer" = $bios.manufacturer
+		"Bios" =  $Bios.SMBIOSBIOSVersion
+		"Model" = $model.model
+		"Serial" = $bios.SerialNumber
+		"Disk" = $disk.model -join ','
+		"Last Logon" = $lastlog.LastLogonDate
+		"BitLocker status" = $Status 
+		"Last Patch date" = $lastpatch.installedon
+		"Build No." = $buildno
+}
+Write-Output $Obj
+}
+    else {
+    }
+}

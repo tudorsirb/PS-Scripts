98bdf78145e120437d303693456556e08e1dffaa
Tudor Sirb <tudor@sirb.net>
2018-11-01 14:14:59 +0200

    fix typo

--- get current users from workstations.ps1
+++ get current users from workstations.ps1
@@ -5,8 +5,7 @@ foreach ($computer in $Computers) {
 if( (Test-Connection $computer.name -Quiet -count 1)) {
 $lastlogon = (Get-WmiObject -Class win32_process -ComputerName $computer.name | Where-Object name -Match explorer).getowner().user
 $hostname = (invoke-command -ComputerName $computer.name -ScriptBlock {hostname})
-$ip = Invoke-Command -computername $computer.name -scriptblock {(Get-CimInstance Win32_NetworkAdapterC
-onfiguration).ipaddress}
+$ip = Invoke-Command -computername $computer.name -scriptblock {(Get-CimInstance Win32_NetworkAdapterConfiguration).ipaddress}
  $Obj = New-Object -TypeName PSObject -Property @{
 		"Current user" = $lastlogon -join ','
 		"Hostname" = $hostname	

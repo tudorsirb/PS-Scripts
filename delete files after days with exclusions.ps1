cdcb4c093f6aaa925d25ad8d88eb832ed33bf9e2
Tudor Sirb <tudor.sirb@morningstar.com>
2021-03-24 10:32:24 +0200

    delete files after specified days and exclude files from list

---
+++ delete files after days with exclusions.ps1
@@ -0,0 +1,19 @@
+﻿$path = 'e:\testfiles'
+$exclusions = @(Get-Content -LiteralPath E:\exclude\exclude.txt) 
+$CurrentDate = Get-Date
+$Daysback = "-30"
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+$filestodelete = Get-ChildItem $path -recurse -Exclude $exclusions |
+Where-Object {$_.LastWriteTime -lt $DatetoDelete } |
+Where-Object {
+    $valid = $True
+    foreach ($exclusion in $exclusions) {
+            If ($_.FullName -match "^$([regex]::Escape($exclusion))") {
+            $valid = $False
+            Break
+        }
+    }   
+    $valid
+} | select -expandproperty fullName
+Write-Host $filestodelete
+Remove-Item -Path $filestodelete -recurse | Where { ! $_.PSIsContainer }
